16cd5e7b534a05b637718175d2d7718d10238740
Chinki <chinki789@gmail.com>
2025-11-16 14:18:07 +0530

    Prepare for Render deployment

--- main.py
+++ main.py
@@ -70,4 +70,6 @@ def generate_report_pdf():
 # -------------------------------
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
